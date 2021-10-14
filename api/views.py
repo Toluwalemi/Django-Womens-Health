9d8a98ae53cdbdbb5f1899c7ca3c99fba36c924b
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-14 03:17:58 +0100

    feature: add view function

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.serializers import PeriodCycleSerializer
+
+
+def ping(request):
+    data = {"ping": "pong!"}
+    return JsonResponse(data)
+
+
+class PeriodCycleList(APIView):
+    def post(self, request, format=None):
+        serializer = PeriodCycleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
