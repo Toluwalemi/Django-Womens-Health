bd5ed7c42f2b6e86eab09f81a8d9a4edcf4089ff
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-14 03:15:48 +0100

    feature: create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class PeriodCycle(models.Model):
+    """
+    Model to store a lady's period cycle data.
+    """
+    last_period_date = models.DateField(help_text="Enter your last period date")
+    cycle_average = models.IntegerField()
+    period_average = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Period Cycles"
