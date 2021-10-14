e282ab0b41822bbab08233e2749a4d255658aba2
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-14 13:20:11 +0100

    feature: add put method test

--- api/tests/api/test_period_cycle_view.py
+++ api/tests/api/test_period_cycle_view.py
@@ -5,6 +5,7 @@ from api.models import PeriodCycle
 
 @pytest.mark.django_db
 def test_add_period_cycle(client):
+    """Ensure the period cycle details entered returns the total created cycle"""
     period_cycle = PeriodCycle.objects.all()
     assert len(period_cycle) == 0
 
@@ -15,12 +16,12 @@ def test_add_period_cycle(client):
             "cycle_average": 25,
             "period_average": 5,
             "start_date": "2020-07-25",
-            "end_date": "2021-10-25"
+            "end_date": "2021-07-25"
         },
         content_type="application/json"
     )
     assert resp.status_code == 201
-    assert resp.data["total_created_cycles"] == 18
+    assert resp.data["total_created_cycles"] == 14
 
     period_cycle = PeriodCycle.objects.all()
     assert len(period_cycle) == 1
@@ -28,6 +29,7 @@ def test_add_period_cycle(client):
 
 @pytest.mark.django_db
 def test_add_movie_invalid_json(client):
+    """Test if an empty json is provided"""
     period_cycle = PeriodCycle.objects.all()
     assert len(period_cycle) == 0
 
@@ -44,6 +46,7 @@ def test_add_movie_invalid_json(client):
 
 @pytest.mark.django_db
 def test_add_period_cycle_invalid_json_keys(client):
+    """Test if all the details are not provided"""
     period_cycle = PeriodCycle.objects.all()
     assert len(period_cycle) == 0
 
@@ -63,6 +66,7 @@ def test_add_period_cycle_invalid_json_keys(client):
 
 @pytest.mark.django_db
 def test_add_period_cycle_to_return_zero(client):
+    """Ensure that create-cycle endpoint returns zero"""
     period_cycle = PeriodCycle.objects.all()
     assert len(period_cycle) == 0
 
@@ -73,12 +77,48 @@ def test_add_period_cycle_to_return_zero(client):
             "cycle_average": 25,
             "period_average": 5,
             "start_date": "2021-10-25",
-            "end_date":  "2020-07-25"
+            "end_date": "2020-07-25"
         },
         content_type="application/json"
     )
     assert resp.status_code == 201
     assert resp.data["total_created_cycles"] == 0
+
+    period_cycle = PeriodCycle.objects.all()
+    assert len(period_cycle) == 1
+
+
+@pytest.mark.django_db
+def test_update_period_cycle(client):
+    """Test to ensure a user can update period details"""
+    period_cycle = PeriodCycle.objects.all()
+    assert len(period_cycle) == 0
+
+    resp_post = client.post(
+        "/womens-health/api/create-cycle/",
+        {
+            "last_period_date": "2020-06-06",
+            "cycle_average": 25,
+            "period_average": 5,
+            "start_date": "2020-07-25",
+            "end_date": "2021-07-25"
+        },
+        content_type="application/json"
+    )
+
+    resp = client.put(
+        "/womens-health/api/create-cycle/1/",
+        {
+            "last_period_date": "2020-06-06",
+            "cycle_average": 25,
+            "period_average": 5,
+            "start_date": "2020-06-25",
+            "end_date": "2021-07-25"
+        },
+        content_type="application/json"
+    )
+    assert resp.status_code == 200
+    assert resp.data["total_created_cycles"] == 15
 
     period_cycle = PeriodCycle.objects.all()
     assert len(period_cycle) == 1
