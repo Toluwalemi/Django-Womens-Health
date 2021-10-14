b2138454564741169a0c63e533a2ebada0b3959b
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-14 03:17:45 +0100

    test: add new tests

---
+++ api/tests/api/test_views.py
@@ -0,0 +1,62 @@
+import pytest
+
+from api.models import PeriodCycle
+
+
+@pytest.mark.django_db
+def test_add_period_cycle(client):
+    period_cycle = PeriodCycle.objects.all()
+    assert len(period_cycle) == 0
+
+    resp = client.post(
+        "/womens-health/api/create-cycle/",
+        {
+            "last_period_date": "2020-06-06",
+            "cycle_average": 25,
+            "period_average": 5,
+            "start_date": "2020-07-25",
+            "end_date": "2021-10-25"
+        },
+        content_type="application/json"
+    )
+    assert resp.status_code == 201
+    assert resp.data["period_average"] == 5
+    assert resp.data["end_date"] == "2021-10-25"
+
+    period_cycle = PeriodCycle.objects.all()
+    assert len(period_cycle) == 1
+
+
+@pytest.mark.django_db
+def test_add_movie_invalid_json(client):
+    period_cycle = PeriodCycle.objects.all()
+    assert len(period_cycle) == 0
+
+    resp = client.post(
+        "/womens-health/api/create-cycle/",
+        {},
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    period_cycle = PeriodCycle.objects.all()
+    assert len(period_cycle) == 0
+
+
+@pytest.mark.django_db
+def test_add_period_cycle_invalid_json_keys(client):
+    period_cycle = PeriodCycle.objects.all()
+    assert len(period_cycle) == 0
+
+    resp = client.post(
+        "/womens-health/api/create-cycle/",
+        {
+            "last_period_date": "2020-06-06",
+            "cycle_average": 25,
+        },
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    period_cycle = PeriodCycle.objects.all()
+    assert len(period_cycle) == 0
