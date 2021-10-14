813ce0fd6495947cdf7a1048673839750711a0bc
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-14 03:17:22 +0100

    feature: add serializers

---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import PeriodCycle
+
+from datetime import datetime
+
+
+class PeriodCycleSerializer(serializers.ModelSerializer):
+    class Meta:
+        last_period_date = serializers.DateField(format=['%Y-%m-%d'], input_formats=['%Y-%m-%d'])
+        start_date = serializers.DateField(input_formats=['%Y%m%d'])
+        end_date = serializers.DateField(input_formats=['%Y%m%d'])
+        model = PeriodCycle
+        fields = ('last_period_date', 'cycle_average', 'period_average',
+                  'start_date', 'end_date',)
