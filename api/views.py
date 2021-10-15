90288c1e5d381935124073c0e5bf7855c94e110e
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-15 13:37:50 +0100

    refactor: remove redundant statement

--- api/views.py
+++ api/views.py
@@ -66,9 +66,7 @@ class CycleEventView(APIView):
             return Response({"msg": "Object not found"}, status=status.HTTP_400_BAD_REQUEST)
         serializer = PeriodCycleSerializer(queryset)
         queryset_params: dict = fetch_serialized_data(serializer)
-
         if date:
-            resp: dict = helper_cycle_event(queryset_params, date)
-            return Response(resp)
+            return Response(helper_cycle_event(queryset_params, date))
 
         return Response({}, status=status.HTTP_400_BAD_REQUEST)
