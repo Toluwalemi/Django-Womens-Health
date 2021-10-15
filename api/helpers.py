ff199544358fc3d5776fa53ed518d5228b556f1f
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-15 05:40:26 +0100

    refactor: refactor helpers

--- api/helpers.py
+++ api/helpers.py
@@ -60,6 +60,48 @@ def fetch_serialized_data(queryset) -> dict:
             'cycle_average': queryset['cycle_average']}
 
 
+def get_period_start_date_lst(strp_lpd, cycle_lst):
+    date_format_full = "%Y-%m-%d %H:%M:%S"
+    period_start_date_lst = []
+    for i in cycle_lst:
+        add_date = str(strp_lpd + timedelta(days=i))
+        dates_strp = datetime.strptime(str(add_date), date_format_full)
+        dates_strf = dates_strp.strftime("%Y-%m-%d")
+        period_start_date_lst.append(dates_strf)
+    return period_start_date_lst
+
+
+def get_ovulation_dates(period_start_date_lst, new_cycle_average):
+    date_format = "%Y-%m-%d"
+    date_format_full = "%Y-%m-%d %H:%M:%S"
+    ovulation_date_lst = []
+    for i in period_start_date_lst:
+        get_date = datetime.strptime(str(i), date_format)
+        add_date = str(get_date + timedelta(days=new_cycle_average))
+        ovu_strp = datetime.strptime(str(add_date), date_format_full)
+        ovu_strf = ovu_strp.strftime("%Y-%m-%d")
+        ovulation_date_lst.append(ovu_strf)
+
+    return ovulation_date_lst
+
+
+def get_fertility_dates(ovulation_date_lst):
+    date_format = "%Y-%m-%d"
+    date_format_full = "%Y-%m-%d %H:%M:%S"
+    fertility_window_lst = []
+    for ovu in ovulation_date_lst:
+        get_date = datetime.strptime(str(ovu), date_format)
+        add_to = str(get_date + timedelta(days=4))
+        sub_from = str(get_date - timedelta(days=4))
+        fert_add_strp = datetime.strptime(str(add_to), date_format_full)
+        fert_add_strf = fert_add_strp.strftime("%Y-%m-%d")
+        fert_sub_strp = datetime.strptime(str(sub_from), date_format_full)
+        fert_sub_strf = fert_sub_strp.strftime("%Y-%m-%d")
+        fertility_window_lst.append(fert_add_strf)
+        fertility_window_lst.append(fert_sub_strf)
+    return fertility_window_lst
+
+
 def helper_cycle_event(queryset_params, date) -> dict:
     """
     Helper function to get the cycle event
@@ -76,36 +118,14 @@ def helper_cycle_event(queryset_params, date) -> dict:
     last_period_date = queryset_params["last_period_date"].value
     cycle_average = queryset_params["cycle_average"].value
     new_cycle_average = math.floor(cycle_average / 2)
-
     strp_lpd = datetime.strptime(str(last_period_date), date_format)
-    period_start_date_lst = []
-    for i in cycle_lst:
-        add_date = str(strp_lpd + timedelta(days=i))
-        dates_strp = datetime.strptime(str(add_date), date_format_full)
-        dates_strf = dates_strp.strftime("%Y-%m-%d")
-        period_start_date_lst.append(dates_strf)
 
+    # get period start date
+    period_start_date_lst = get_period_start_date_lst(strp_lpd, cycle_lst)
     # get ovulation dates
-    ovulation_date_lst = []
-    for i in period_start_date_lst:
-        get_date = datetime.strptime(str(i), date_format)
-        add_date = str(get_date + timedelta(days=new_cycle_average))
-        ovu_strp = datetime.strptime(str(add_date), date_format_full)
-        ovu_strf = ovu_strp.strftime("%Y-%m-%d")
-        ovulation_date_lst.append(ovu_strf)
-
+    ovulation_date_lst = get_ovulation_dates(period_start_date_lst, new_cycle_average)
     # get fertility window
-    fertility_window_lst = []
-    for ovu in ovulation_date_lst:
-        get_date = datetime.strptime(str(ovu), date_format)
-        add_to = str(get_date + timedelta(days=4))
-        sub_from = str(get_date - timedelta(days=4))
-        fert_add_strp = datetime.strptime(str(add_to), date_format_full)
-        fert_add_strf = fert_add_strp.strftime("%Y-%m-%d")
-        fert_sub_strp = datetime.strptime(str(sub_from), date_format_full)
-        fert_sub_strf = fert_sub_strp.strftime("%Y-%m-%d")
-        fertility_window_lst.append(fert_add_strf)
-        fertility_window_lst.append(fert_sub_strf)
+    fertility_window_lst = get_fertility_dates(ovulation_date_lst)
 
     answer_dict = {}
     if date in period_start_date_lst:
