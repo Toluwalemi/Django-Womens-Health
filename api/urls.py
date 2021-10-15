971c04b862d9a0ead4bf4999b07dd6fd7a0c6ce1
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-15 02:50:33 +0100

    feature: create path for  view function

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PeriodCycleListView, ping, PeriodCycleDetail
+from .views import PeriodCycleList, ping, PeriodCycleDetail, CycleEventView
 
 urlpatterns = [
     path('ping/', ping, name="ping"),
-    path('create-cycle/', PeriodCycleListView.as_view()),
+    path('create-cycle/', PeriodCycleList.as_view()),
     path('create-cycle/<int:pk>/', PeriodCycleDetail.as_view()),
+    path('cycle-event/', CycleEventView.as_view()),
 ]
